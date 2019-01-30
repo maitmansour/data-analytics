0c7415e997968978806335cf4758864e452f7a21
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2019-01-30 13:45:09 +0100

    read data from file, add comments

--- data-mining/analyse.py
+++ data-mining/analyse.py
@@ -7,16 +7,21 @@
 # librairies import
 import pandas as pd
 
-#read input text and put data inside a data frame
+# read input text and put data inside a data frame
 fruits = pd.read_csv('data/fruit_data_with_colors.txt', sep='\t')
 
+# data head
+print("\nFruits Head \n")
 print( fruits .head())
 
 # print nb of instances and features
+print("\nNumber of instances and Features \n")
 print( fruits .shape)
 
 # print feature types
+print("\nFeatures Types \n")
 print( fruits .dtypes)
 
 # print balance between classes
+print("\nBalance between classes \n")
 print( fruits .groupby('fruit_name').size())
