422566ae60632dd16aa1ea9a337dd66b4342755d
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2019-01-30 15:28:35 +0100

    generate hits

--- data-mining/analyse.py
+++ data-mining/analyse.py
@@ -6,6 +6,9 @@
 
 # librairies import
 import pandas as pd
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import numpy as np
 
 # read input text and put data inside a data frame
 fruits = pd.read_csv('data/fruit_data_with_colors.txt', sep='\t')
@@ -25,3 +28,18 @@ print( fruits .dtypes)
 # print balance between classes
 print("\nBalance between classes \n")
 print( fruits .groupby('fruit_name').size())
+
+# Declaration
+cmap = cm.get_cmap('gnuplot')
+fruits_name=['mass','width','height','color_score']
+X=fruits[fruits_name]
+y=fruits['fruit_label']
+
+# Scatter Matrix
+pd.plotting.scatter_matrix(X, c = y, marker = 'o', s=40, hist_kwds={'bins':15}, figsize=(9,9),cmap=cmap)
+
+# Generate Hists
+for attr in fruits_name:
+	pd.DataFrame({k: v for k, v in fruits.groupby('fruit_name')[attr]}). plot. hist (stacked=True)
+	plt . suptitle (attr)
+	plt . savefig ('plot/fruits_histogram_'+attr)
