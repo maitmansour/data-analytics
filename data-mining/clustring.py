b89cb9152024bc5da6c788667c92cf100d8c2dae
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2019-02-05 17:02:14 +0100

    clustring methods

--- data-mining/clustring.py
+++ data-mining/clustring.py
@@ -5,9 +5,6 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import r2_score
-from importlib.machinery import SourceFileLoader
-R_square_clustering = SourceFileLoader("r_square", "scripts/R_square_clustering.py").load_module()
 
 # read input text and put data inside a data frame
 fruits = pd.read_csv('data/fruit_data_with_colors.txt', sep='\t')
@@ -16,8 +13,6 @@ X.drop(["fruit_name","fruit_subtype","fruit_label"], axis = 1, inplace = True)
 
 scaler = MinMaxScaler()
 X_norm=scaler.fit_transform(X)
-
-# TODO : enlever label, et appliquer un filtre de normalisation pour obtenir x_norm
 
 # Plot clusters
 lst_kmeans = [KMeans(n_clusters=n) for n in range(3,6)]
@@ -64,20 +59,4 @@ ax.set_zlabel('color_score')
 ax. set_title ('Ground Truth')
 ax. dist = 12
 plt . savefig ('plot/k-means_ground_truth')
-plt . close ( fig )
-
-# metrique r2
-lst_k=range(2,10)
-lst_rsq = []
-for k in lst_k:
-	est=KMeans(n_clusters=k)
-	est . fit (X_norm)
-	lst_rsq.append(R_square_clustering.r_square(X_norm, est.cluster_centers_,est.labels_,k))
-
-fig = plt. figure ()
-plt . plot(lst_k, lst_rsq, 'bx-' )
-plt . xlabel('k')
-plt . ylabel('RSQ')
-plt . title ('The Elbow Method showing the optimal k')
-plt . savefig ('plot/k-Elbow-Method')
 plt . close ( fig )
