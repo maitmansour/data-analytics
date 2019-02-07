6a1af99343bc4dd3097609d6529b70786aa51386
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2019-02-07 09:18:03 +0100

    read data from arrf file

--- eurostat-app/tp2-1.2.py
+++ eurostat-app/tp2-1.2.py
@@ -3,17 +3,29 @@
 
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
+from scipy.io import arff
+import pandas as pd
+
+# read data from file
+data = arff.loadarff('data/pull/vote.arff')
+vote = pd.DataFrame(data[0])
+
 
 # convert categorical values into one-hot vectors and ignore ? values
 # corresponding to missing values
 # ex: handicapped-infants=y -> [1,0], handicapped-infants=n -> [0,1], handicapped-infants=? -> [0,0]
 vote_one_hot = pd.get_dummies(vote)
 vote_one_hot.drop(vote_one_hot.filter(regex='_\?$',axis=1).columns,axis=1,inplace=True)
+print(vote_one_hot)
+
+
+#calculer les itemsets avec apriori
+#calculer les itemset frequet freequent_item_sets = apriori(vote one hot)
 
 
 
 
 # check that there is no rule implying Republicans
-filter(lambda x: "Class_'republican'" in x,rules['antecedents'])
-filter(lambda x: "Class_'republican'" in x,rules['consequents'])
+#filter(lambda x: "Class_'republican'" in x,rules['antecedents'])
+#filter(lambda x: "Class_'republican'" in x,rules['consequents'])
 
