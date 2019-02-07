931f83f548c7a219dd2d45333e31643a82ae270a
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2019-02-07 09:34:51 +0100

    show frequence item sets

--- eurostat-app/tp2-1.2.py
+++ eurostat-app/tp2-1.2.py
@@ -14,9 +14,29 @@ vote = pd.DataFrame(data[0])
 # convert categorical values into one-hot vectors and ignore ? values
 # corresponding to missing values
 # ex: handicapped-infants=y -> [1,0], handicapped-infants=n -> [0,1], handicapped-infants=? -> [0,0]
+
+# Que proposez-vous pour tenir compte de cette absence de valeurs pour ne pas voir apparaitre des règles non pertinentes ?
+# Afin d'éviter l'apparence des régles non pertinantes, on va supprimer les données dont la classe est absente
 vote_one_hot = pd.get_dummies(vote)
 vote_one_hot.drop(vote_one_hot.filter(regex='_\?$',axis=1).columns,axis=1,inplace=True)
-print(vote_one_hot)
+
+# Affichage des données
+#print(vote_one_hot)
+
+# item_sets contienne l'ensemble des items set, avec le numero du ligne qui donne cet itemset, 
+# le nombre des item sets qu'on a obtenu est : 118
+#Donnez leur statistique en fonction du nombre d'items qu'ils contiennent 
+
+
+frequent_itemsets=apriori(vote_one_hot, min_support=0.4)
+
+print(frequent_itemsets)
+
+
+# ici, on affiche plus de détails sur les item sets
+frequent_itemsets_details = apriori(vote_one_hot, min_support=0.4, use_colnames=True)
+frequent_itemsets_details['length'] = frequent_itemsets_details['itemsets'].apply(lambda x: len(x))
+
 
 
 #calculer les itemsets avec apriori
